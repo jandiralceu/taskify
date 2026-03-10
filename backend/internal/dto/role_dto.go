a8c501f724450ea5d23264afacb1e6bd8c8c4535
Jandir A. Cutabiala <jandiralceu@gmail.com>
2026-03-10 10:25:33 -0300

    feat: add trigger to automatically update updated_at column

--- backend/internal/dto/role_dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type CreateRoleRequest struct {
-	Name        string `json:"name" binding:"required,min=3,max=50"`
-	Description string `json:"description" binding:"required,min=3"`
-}
